3c6dec490afca22405c13f48e5d51f007ca84a21
crich926 <36669147+crich926@users.noreply.github.com>
2018-02-20 19:56:41 -0500

    Add files via upload

---
+++ Slideshow.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!-- saved from url=(0046)https://www.ubs.iastate.edu/slideshow_2015.php -->
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+
+<title>Slideshow</title>
+<link rel="stylesheet" href="https://www.ubs.iastate.edu/css/new_slideshow_2015.css" type="text/css" media="screen">        
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script><style></style>
+        
+</head>
+
+<body>
+
+
+<script>
+	$(function () {
+		$("#slideshow").responsiveSlides({
+			maxwidth: 1200,
+			speed: 800,
+			nav: true
+		});
+	});
+</script>
+
+<div id="wrapper">
+	<div class="callbacks_container">
+        <ul class="rslides rslides1" id="slideshow" style="max-width: 1200px;">
+
+<li id="rslides1_s0" class="rslides1_on" style="display: list-item; float: left; position: relative; opacity: 1; z-index: 2; transition: opacity 800ms ease-in-out;"><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display: block;" id="spacer"><a href="http://www.isubookstore.com/SiteText.aspx?id=12211" target="_parent"><img class="slide" src="./Slideshow_files/default_clothinggifts.gif"></a><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display:block;" id="spacer"><div class="caption">More than Books -  We have everything you need to cheer on the Cyclones!</div></li><li id="rslides1_s1" class="" style="display: list-item; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 800ms ease-in-out;"><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display: block;" id="spacer"><a href="http://www.isubookstore.com/shop/office/cys-deals-supplies" target="_parent"><img class="slide" src="./Slideshow_files/default_officeartsupplies.gif"></a><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display:block;" id="spacer"><div class="caption">Office and Art Supplies Galore!</div></li><li id="rslides1_s2" class="" style="display: list-item; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 800ms ease-in-out;"><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display: block;" id="spacer"><a href="http://www.isubookstore.com/SelectTermDept.aspx?" target="_parent"><img class="slide" src="./Slideshow_files/default_compareprices.gif"></a><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display:block;" id="spacer"><div class="caption">Compare and Buy All In One Click.</div></li><li id="rslides1_s3" class="" style="display: list-item; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 800ms ease-in-out;"><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display: block;" id="spacer"><a href="http://www.techcyte.isubookstore.com/home.aspx" target="_parent"><img class="slide" src="./Slideshow_files/default_techcyte.gif"></a><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display:block;" id="spacer"><div class="caption">Tech CYte - Educational pricing on the best computer brands and software!</div></li><li id="rslides1_s4" class="" style="display: list-item; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 800ms ease-in-out;"><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display: block;" id="spacer"><a href="http://www.isubookstore.com/MerchList.aspx?ID=29348" target="_parent"><img class="slide" src="./Slideshow_files/default_cysdeals.gif"></a><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display:block;" id="spacer"><div class="caption">Save with Cy's Deals!</div></li><li id="rslides1_s5" class="" style="display: list-item; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 800ms ease-in-out;"><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display: block;" id="spacer"><a href="http://www.isubookstore.com/MerchList.aspx?ID=15007" target="_parent"><img class="slide" src="./Slideshow_files/default_gbooks.gif"></a><img class="slide" src="./Slideshow_files/spacer.gif" style="height:0px; display:block;" id="spacer"><div class="caption">General Books for your reading pleasure</div></li>			
+    </ul><a href="https://www.ubs.iastate.edu/slideshow_2015.php#" class="rslides_nav rslides1_nav prev">Previous</a><a href="https://www.ubs.iastate.edu/slideshow_2015.php#" class="rslides_nav rslides1_nav next">Next</a>
+</div>
+    <!--<div class="nav-container">
+        <ul class="directional">
+        <!-- These spans show the directional arrow buttons on side of the slideshow -->
+            <!--<li class="prev"><span><span></span></span></li>
+            <li class="next"><span><span></span></span></li>
+        </ul>
+        <ul class="controls">
+             <li class="slideshow"><span></span></li><!-- This span shows the play/pause button -->
+            <!-- These spans show the empty circles that represent each slide -->
+            <!--<li><span></span></li>
+            <li><span></span></li>
+            <li><span></span></li>
+            <li><span></span></li>
+            <li><span></span></li>
+            <li><span></span></li>
+                    </ul>
+    </div>-->
+</div>
+
+    
+<!--Do not change the javascript below  --> 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://www.ubs.iastate.edu/js/responsiveslides.js"></script>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script src="https://ssl.google-analytics.com/ga.js" type="text/javascript"></script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8045686-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body></html>
