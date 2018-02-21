5a257cb84a8e60d269914a24dd02e35858bfca9a
crich926 <36669147+crich926@users.noreply.github.com>
2018-02-20 19:30:41 -0500

    Update sss.js

--- sss.js
+++ sss.js
@@ -2,6 +2,10 @@
 
 ;(function($, window, document, undefined ) {
 
+$('.slider').sss({
+slideShow : true, // Set to false to prevent SSS from automatically animating.startOn : 0, // Slide to display first. Uses array notation (0 = first slide).transition : 400, // Length (in milliseconds) of the fade transition.speed : 3500, // Slideshow speed in milliseconds.showNav : true // Set to false to hide navigation arrows.
+});
+	
 $.fn.sss = function(options) {
 
 // Options
